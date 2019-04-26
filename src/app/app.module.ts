276f8f015e49343217359683b98c47799c4200a1
fernandoluchiari <fe.luchiari@gmail.com>
2019-04-26 12:27:34 -0300

    Estudando angular + bootstrap + angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
-import {MatFormFieldModule, MatInputModule, MatCardModule} from '@angular/material';
+import {MatFormFieldModule, MatInputModule,
+  MatCardModule, MatIcon, MatIconModule,
+  MatToolbarModule, MatSidenavModule,  MatListModule} from '@angular/material';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegisterComponent } from './register/register.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import {MatButtonModule} from '@angular/material';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
@@ -19,7 +28,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     FormsModule,
     MatInputModule,
     BrowserAnimationsModule,
-    MatCardModule
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    RouterModule.forRoot([
+      {path: '', component: LoginComponent},
+      {path: 'register', component: RegisterComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
