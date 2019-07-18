f75cf9d2e7030f42de4d0f23a1c4700f3e5b2b9d
Fernando Luchiari <luchiari.fernando@gmail.com>
2019-07-17 23:20:49 -0300

    criação api de create car

--- backend/admin.js
+++ backend/admin.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const multer  = require('multer');
+const User = require('./models/car');
 const router = express.Router();
 
 const storage = multer.diskStorage({
@@ -17,6 +18,21 @@ const upload = multer({
 
 router.post('/save-image', upload.single('file'), (req,res,next) => {
     res.status(201).json('image uploaded');
+});
+
+router.post('/create-car', (req,res,next) => {
+    const car = new Car ({
+        brand: req.body.brand,
+        model: req.body.model,
+        power: req.body.power,
+        seats: req.body.seats,
+        imgUrl: req.body.imgUrl
+    });
+    car.save().then(result => {
+        return res.status(200).json({message: 'car created'});
+    }).catch(error => {
+        console.log(error);
+    })
 });
 
 module.exports = router;
---
+++ backend/models/car.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const carSchema = mongoose.Schema({
+    brand:String,
+    model: String,
+    power: String,
+    seats: Number,
+    imgUrl: String
+
+});
+
+module.exports = mongoose.model('Car', userSchema);
